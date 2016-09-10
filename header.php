48001a6aeee903b79bcf5507dd7e483a54021433
Maakep <schon.fredrik@gmail.com>
2016-09-10 11:09:55 +0200

    img-ify + variables #1 #2

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
-<div class="header">
-	<div class="header-image">
-		<a href="."><img src="http://placehold.it/1024x300" /></a>
+<a href=".">
+	<div class="header">
 	</div>
-</div>
+</a>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<link rel="shortcut icon" type="image/png" href="favicon.png"/>
+		<link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
 		<link rel="stylesheet" type="text/css" href="style/main.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<script src="js/js.js"></script>

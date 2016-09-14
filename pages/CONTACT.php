94eaf130c6ef5e03a929891ba0f56211d25ca1ce
Maakep <schon.fredrik@gmail.com>
2016-09-14 19:04:01 +0200

    Change email address, fix bug with input field being link and not readonly

--- pages/CONTACT.php
+++ pages/CONTACT.php
@@ -1,10 +1,10 @@
 <div class="contact-content flex justify-around icon-imgs padding-100 text-center">
 	<div class="contact-item">
-		<a href="mailto:emilallin@hotmail.com">
-			<img src="./img/outlook.png">
+		<a href="mailto:emil.hallin93@gmail.com">
+			<img src="./img/Gmaillogo2014.png">
 			<br>
-			<input onclick="this.select();" value="emilallin@hotmail.com" class="contact-input">
 		</a>
+		<input onclick="this.select();" value="emil.hallin93@gmail.com" class="contact-input" readonly>
 	</div>
 	<div class="contact-item">
 		<a target="_blank" href="https://www.linkedin.com/in/emil-hallin-8b74a3a8">

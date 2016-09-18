bebe1fb57d99f6736c8546b2803d8545bd5f07fa
Maakep <schon.fredrik@gmail.com>
2016-09-18 14:36:35 +0200

    Padding on contact page, variables for contact page

--- pages/CONTACT.php
+++ pages/CONTACT.php
@@ -1,34 +1,42 @@
-<div class="contact-content">
-	<div class="about">
-		<p>
-			Graduated from Malmö Högskola 2016 with a degree in Game Development.
+<?php 
+$twitter = 'UrbanHelsing';
+$linkedin = array("url" => "https://www.linkedin.com/in/emil-hallin-8b74a3a8", "name" => "Emil Hallin");
+$email = "emil.hallin93@gmail.com";
+//html supported
+$aboutText = '
+Graduated from Malmö Högskola 2016 with a degree in Game Development.
 			Regular participant in Ludum Dare and other game jams.
 			Enjoy playing games.
 			Enjoy making games.
+';
+
+?>
+
+<div class="padding-100">
+		<p>
+			<? echo $aboutText; ?>
 		</p>
 	</div>
-
-	<div class="flex justify-around icon-imgs padding-100 text-center">
-		<div class="contact-item">
-			<a href="mailto:emil.hallin93@gmail.com">
-				<img src="./img/Gmaillogo2014.png">
-				<br>
-			</a>
-			<input onclick="this.select();" value="emil.hallin93@gmail.com" class="contact-input" readonly>
-		</div>
-		<div class="contact-item">
-			<a target="_blank" href="https://www.linkedin.com/in/emil-hallin-8b74a3a8">
-				<img src="./img/linkedin.png">
-				<br>
-				<i>Emil Hallin</i>
-			</a>
-		</div>
-		<div class="contact-item">
-			<a target="_blank" href="https://twitter.com/UrbanHelsing">
-				<img src="./img/twitterbird.png">
-				<br>
-				<i>@UrbanHelsing</i>
-			</a>
-		</div>
+<div class="contact-content flex justify-around icon-imgs padding-100 text-center">
+	<div class="contact-item">
+		<a href="mailto:<? echo $email; ?>">
+			<img src="./img/Gmaillogo2014.png">
+			<br>
+		</a>
+		<input onclick="this.select();" value="<? echo $email; ?>" class="contact-input" readonly>
+	</div>
+	<div class="contact-item">
+		<a target="_blank" href="<? echo $linkedin["url"]; ?>">
+			<img src="./img/linkedin.png">
+			<br>
+			<i><? echo $linkedin["name"]; ?></i>
+		</a>
+	</div>
+	<div class="contact-item">
+		<a target="_blank" href="https://twitter.com/<? echo $twitter; ?>">
+			<img src="./img/twitterbird.png">
+			<br>
+			<i>@<? echo $twitter; ?></i>
+		</a>
 	</div>
 </div>
